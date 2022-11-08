647862cb2213c1730b3bf0991df7ae865f81d0af
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-11-08 21:59:54 +1000

    contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar"
 import Landing from "./Landing"
+import Contact from "./Contact"
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
