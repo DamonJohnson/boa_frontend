56a1e28d3bbfa3388a278575db43d7272782b953
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-12-20 21:46:45 +1000

    scaffold for product1 page

--- src/components/Products/Product1.jsx
+++ src/components/Products/Product1.jsx
@@ -1,18 +1,192 @@
-import React from 'react'
+import React from "react"
 
 const Product1 = () => {
-    return (
-        <section>
-            <div className="py-4 lg:py-8 px-4 mx-auto max-w-screen-2xl">
-                <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-secondary">
-                    Reusable Pallet Wrap
-                </h1>
+  return (
+    <section>
+      <div className="container px-6 py-10 mx-auto">
+        <div className="lg:-mx-6 lg:flex lg:items-center">
+          <img
+            className="object-cover object-center lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem]"
+            src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80&"
+                      alt=""
+          ></img>
+          <div className="mt-8 lg:w-1/2 lg:px-6 lg:mt-0">
 
+            <h1 className="text-3xl font-semibold text-secondary lg:text-4xl lg:w-100">
+              1.5m Reusable Pallet Wrap
+            </h1>
 
+            <ul className="max-w-lg mt-6 secondary">
+                          <li>Height: 1.5m</li>
+                          <li>Pallet Size: 1.2 x 1.2m</li>
+                          <li>Mesh Length: 4m</li>
+                          <li>Material: PVC Mesh</li>
+            </ul>
 
+            
+
+                      <div className="flex items-center justify-between mt-12 lg:justify-start">
+                           
+                          <div class="flex justify-center">
+  <div>
+    <div class="dropdown relative">
+      <button
+        class="
+          dropdown-toggle
+          px-6
+          py-2.5
+          bg-secondary
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg active:text-white
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+        "
+        type="button"
+        id="dropdownMenuButton1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        QTY
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="caret-down"
+          class="w-2 ml-2"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512"
+        >
+          <path
+            fill="currentColor"
+            d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+          ></path>
+        </svg>
+      </button>
+      <ul
+        class="
+          dropdown-menu
+          min-w-max
+          absolute
+          hidden
+          bg-white
+          text-base
+          z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+        aria-labelledby="dropdownMenuButton1"
+      >
+        <li>
+          <a
+            class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+            href="#"
+            >Action</a
+          >
+        </li>
+        <li>
+          <a
+            class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+            href="#"
+            >Another action</a
+          >
+        </li>
+        <li>
+          <a
+            class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+            href="#"
+            >Something else here</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+                          
+
+                    <button
+                href="/product4"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300"
+              >
+                Add to cart
+                <svg
+                  aria-hidden="true"
+                  className="w-4 h-4 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>        
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Product1
