74438e598d9f6781c9ce1d49f5ed9d93c4b58fb7
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2023-05-15 20:29:20 +1000

    changed wording of success message

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -11,8 +11,7 @@ const Success = () => {
           email with your order details shortly.
           </p>
         <p className="pt-2 text-center px-8">
-          We appreciate your business and look forward to serving
-          you again in the future. Thank you for choosing Boa Wraps!
+          We appreciate your business and thank you for choosing Boa Wraps!
         </p>
       </body>
     </div>
