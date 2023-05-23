a7a4a070542daa5ceca03cbb3ec87f4a8841d877
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2023-05-23 14:06:54 +1000

    fixed frontend price

--- src/data/Products.jsx
+++ src/data/Products.jsx
@@ -3,7 +3,7 @@ export const products = [
         id: "001",
         name: "Boa Reusable Pallet Wrap",
         height: "1200",
-        price: 50,
+        price: 9700,
         imageSrc: "https://res.cloudinary.com/damonjohnson/image/upload/v1672232434/Boa/Untitled_design_o6a2z9.png",
         imageAlt: 'Reusable pallet wrap front on view, colour black, height 1.2m',
     },
@@ -11,7 +11,7 @@ export const products = [
         id: "002",
         name: "Boa Reusable Pallet Wrap",
         height: "1500",
-        price: 50,
+        price: 12700,
         imageSrc: "https://res.cloudinary.com/damonjohnson/image/upload/v1672232434/Boa/Untitled_design_o6a2z9.png",
         imageAlt: 'Reusable pallet wrap front on view, colour black, height 1.5m',
     }
